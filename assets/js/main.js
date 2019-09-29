498fa2df034e0b8109a983e270ccfae93f361406
techgurll <ololadeoselu@yahoo.com>
2019-09-29 15:53:07 +0100

    no chnages made yet

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,56 +1,28 @@
 $(function(){
-    var includes = $('[data-include]');
-    jQuery.each(includes, function(){
-      var file = '../components/' + $(this).data('include') + '.html';
-      $(this).load(file);
-    });
+  var includes = $('[data-include]');
+  jQuery.each(includes, function(){
+    var file = '../components/' + $(this).data('include') + '.html';
+    $(this).load(file);
   });
-
-
-  $(document).ready(function(){
-
-    $(".company-subnav-1").hide(); //Hide the navigation bar first
-  
-      $(window).scroll(function () {  //Listen for the window's scroll event
-          if (isScrolledAfterElement(".companysection2")) { //if it has scrolled beyond the #content elment
-              $('.company-subnav-1').fadeIn();  //Show the navigation bar
-              
-
-          } else {
-              $('.company-subnav-1').fadeOut(); //Else hide it
-          }
-      });
-  
-    //Function that returns true if the window has scrolled beyond the given element
-    function isScrolledAfterElement(elem) {
-      var $elem = $(elem);
-      var $window = $(window);
-
-      var docViewTop = $window.scrollTop();
-      var docViewBottom = docViewTop + $window.height();
-
-      var elemTop = $elem.offset().top;
-
-      return elemTop <= docViewBottom;
-  }
 });
+
+
+
+// Hide Then Later Show Navbar
 
 $(document).ready(function(){
 
-  $(".company-subnav-1").hide(); //Hide the navigation bar first
-
-    $(window).scroll(function () {  //Listen for the window's scroll event
-        if (isScrolledBeforeElement(".companysection2")) { //if it has scrolled beyond the #content elment
-            $('.company-subnav-1').fadeOut();  //Show the navigation bar
-            
+  $(".company-subnav-1").hide(); 
+    $(window).scroll(function () {  
+        if (isScrolledAfterElement(".companysection2")) { 
+            $('.company-subnav-1').fadeIn();  
 
         } else {
-            $('.company-subnav-1').fadeIn(); //Else hide it
+            $('.company-subnav-1').fadeOut(); 
         }
     });
 
-
-  //Function that returns true if the window has scrolled beyond the given element
+ 
   function isScrolledAfterElement(elem) {
     var $elem = $(elem);
     var $window = $(window);
@@ -63,3 +35,4 @@ $(document).ready(function(){
     return elemTop <= docViewBottom;
 }
 });
+
