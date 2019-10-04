c833568bd46817a5835c3fde2140e3c43e5a2c53
lawal mukhtar olasukomi <lmukhtar29@gmail.com>
2019-10-04 14:10:36 +0100

    Hopper.com people page fixing

--- assets/js/main.js
+++ assets/js/main.js
@@ -61,3 +61,30 @@ window.onclick = function(event) {
     modal.style.display = "none";
   }
 }
+
+
+$(document).ready(function(){
+
+  $('.people-subnav-1').hide();  
+    $(window).scroll(function () {  
+      if (isScrolledAfterElement(".welcome")) { 
+        $('.people-subnav-1').fadeIn();  
+
+      } else {
+        $('.people-subnav-1').fadeOut(); 
+      }
+    });
+
+ 
+  function isScrolledAfterElement(elem) {
+    var $elem = $(elem);
+    var $window = $(window);
+
+    var docViewTop = $window.scrollTop();
+    var docViewBottom = docViewTop + $window.height();
+
+    var elemTop = $elem.offset().top;
+
+    return elemTop <= docViewBottom;
+}
+});
