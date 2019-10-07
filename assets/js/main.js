b7cdde14597b1978f22f76536e058afab2928995
techgurll <ololadeoselu@yahoo.com>
2019-10-07 09:38:49 +0100

    homepage-cta-modal/sticky-subnav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,46 @@
-$(function(){
-    var includes = $('[data-include]');
-    jQuery.each(includes, function(){
-      var file = '../components/' + $(this).data('include') + '.html';
-      $(this).load(file);
+$(function() {
+  var includes = $("[data-include]");
+  jQuery.each(includes, function() {
+    var file = "../components/" + $(this).data("include") + ".html";
+    $(this).load(file);
   });
 });
 
-
-
 // Hide Then Later Show Navbar
+
+$(document).ready(function() {
+  $(".company-subnav-1").hide();
+  $(window).scroll(function() {
+    if (isScrolledAfterElement(".companysection2")) {
+      $(".company-subnav-1").fadeIn();
+    } else {
+      $(".company-subnav-1").fadeOut();
+    }
+  });
+
+  function isScrolledAfterElement(elem) {
+    var $elem = $(elem);
+    var $window = $(window);
+
+    var docViewTop = $window.scrollTop();
+    var docViewBottom = docViewTop + $window.height();
+
+    var elemTop = $elem.offset().top;
+
+    return elemTop <= docViewBottom;
+  }
+});
+
 
 $(document).ready(function(){
 
-  $('.company-subnav-1').hide();  
+  
     $(window).scroll(function () {  
-        if (isScrolledAfterElement(".companysection2")) { 
-            $('.company-subnav-1').fadeIn();  
+        if (isScrolledAfterElement(".col-12 sectionPhoto")) { 
+            $('.homepage-subnav').fadeIn();  
 
         } else {
-            $('.company-subnav-1').fadeOut(); 
+            $('.homepage-subnav').fadeOut(); 
         }
     });
 
@@ -26,7 +48,6 @@ $(document).ready(function(){
   function isScrolledAfterElement(elem) {
     var $elem = $(elem);
     var $window = $(window);
-
     var docViewTop = $window.scrollTop();
     var docViewBottom = docViewTop + $window.height();
 
